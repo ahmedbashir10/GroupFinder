f28bd10222a2bc2bbe2a0a93fb9705840936c180
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2024-02-05 14:04:02 +0100

    all the components are listed here

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import WelcomeScreen from "./Welcome";
+import EmailScreen from "./EmailScreen";
+import CourseIDScreen from "./CourseID";
+import PreferencesScreen from "./PreferencesScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [currentScreen, setCurrentScreen] = useState("Welcome");
+
+  const renderScreen = () => {
+    switch (currentScreen) {
+      case "Welcome":
+        return <WelcomeScreen onNavigate={() => setCurrentScreen("Email")} />;
+      case "Email":
+        return <EmailScreen onNavigate={() => setCurrentScreen("CourseID")} />;
+      case "CourseID":
+        return (
+          <CourseIDScreen
+            onNavigate={() => setCurrentScreen("PreferencesScreen")}
+          />
+        );
+      case "PreferencesScreen":
+        return (
+          <PreferencesScreen onNavigate={() => setCurrentScreen("Email")} />
+        );
+      // Add more cases for other screens
+      default:
+        return null;
+    }
+  };
+
+  return <View style={styles.container}>{renderScreen()}</View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
