7ed317f1e28b73303e6dbfecd2ecbc812da14e35
ismailwedihaji <ismailwedihaji@gmail.com>
2024-03-15 16:34:57 +0100

    updated .env and readme

--- native/dbConfig.js
+++ native/dbConfig.js
@@ -3,14 +3,14 @@ import 'firebase/compat/auth'
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyALPHkBWqrIV-LHaesTSPH3QRhZnn69r80",
-  authDomain: "groupfinder-ec397.firebaseapp.com",
-  projectId: "groupfinder-ec397",
-  storageBucket: "groupfinder-ec397.appspot.com",
-  messagingSenderId: "1035903508310",
-  appId: "1:1035903508310:web:ed4b19cc0de5372052c0f7",
-  measurementId: "G-116KBEN2H6",
-}
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
 
 
 
